c8d90ad195b6d3556d9d9d34c3207b34cbc1bc51
Gleb Vilkha <vilkha34@gmail.com>
2025-06-24 11:07:43 +0300

    удалил не нужный пробел в приветсвие

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -7,7 +7,7 @@ public class Cli {
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         String name = scanner.nextLine();
-        System.out.println("Hello, " + name + " !");
+        System.out.println("Hello, " + name + "!");
         return name;
     }
 }
